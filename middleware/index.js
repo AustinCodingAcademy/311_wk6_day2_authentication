24ad88e6b06794720805c9afbf85026ee384df17
jonathan gregis <jongregis@jonathans-MacBook-Pro-2.local>
2019-10-04 14:57:43 -0500

    added static pages

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,26 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const logger = () => {}
+const logger = (req, res, next) => {
+  console.log("Logging route:", req.path, new Date().toISOString());
+  next();
+};
 
-const authenticate = () => {}
+const authenticate = (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const [bearer, token] = authHeader.split(" ");
+    const decoded = jwt.verify(token, "secret");
+    if (!decoded) {
+      throw new error("Invalid token");
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
+};
 
 module.exports = {
   logger,
   authenticate
-}
+};
--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,24 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating mysql connection...')
+      console.log("creating mysql connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+        host: "35.238.252.188",
+        user: "root",
+        password: "Buttion1",
+        database: "admin"
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
