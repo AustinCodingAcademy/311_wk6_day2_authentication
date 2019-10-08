ca21cc554c622f906fadaa05cc85e08a3e8eba53
jonathan gregis <jongregis@jonathans-MacBook-Pro-2.local>
2019-10-08 09:50:07 -0500

    final save

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const bodyParser = require("body-parser");
 const usersRouter = require("./routers/users");
 const authRouter = require("./routers/auth");
 const { logger } = require("./middleware");
+const passport = require("passport");
+const initializePassport = require("./passport-config");
 
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
-app.use("/auth", express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Welcome to our server!");
