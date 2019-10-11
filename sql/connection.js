d581e95f154d145632808dbdc2822c50b973410f
fagner henrique flores <henriquefagner@hotmail.com>
2019-10-10 20:51:56 -0500

    henrique 311_wk6_day2_authentication

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
+        host: '34.70.189.224',
         user: 'root',
-        password: 'password',
+        password: 'lexilaura123',
         database: 'admin'
       })
 
