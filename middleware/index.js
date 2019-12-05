8eccfcb3dcb16211fd7980e5779e263111a46c46
Alex Munoz <anmunoz24@gmail.com>
2019-12-04 19:17:47 -0800

    authentication

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,31 @@
 const jwt = require('jsonwebtoken')
 
-const logger = () => {}
 
-const authenticate = () => {}
+const logger = (req, res, next) => {
+  console.log('Logging route:, req.path, newDate().toISOString()')
+  next();
+}
 
+const authenticate = (req, res, next) => {
+  let header = req.headers['authorization']
+  console.log(header)
+  header = header.split(' ')
+  console.log(header)
+
+  const token = header[1]
+
+  jwt.verify(token, 'secret', function(err, decoded){
+    if (err) {
+      console.log("Not Authorized")
+    } else {
+      req.user = decoded
+    }
+    console.log(decoded)
+  })
+  next(); 
+}
+
+  
 module.exports = {
   logger,
   authenticate
