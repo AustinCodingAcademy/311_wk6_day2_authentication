1558573725a52fd447234a85e4408b3d16bb4889
Eugene Ignatov <ignatovyevgen@gmail.com>
2019-12-04 21:08:22 -0600

    authotications

--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,24 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 class Connection {
-  constructor() {
-    if (!this.pool) {
-      console.log('creating mysql connection...')
-      this.pool = mysql.createPool({
-        connectionLimit: 100,
-        host: '35.184.42.200',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+	constructor() {
+		if (!this.pool) {
+			console.log('creating mysql connection...');
+			this.pool = mysql.createPool({
+				connectionLimit: 100,
+				host: '35.225.150.3',
+				user: 'root',
+				password: 'Andree_1986',
+				database: 'admin'
+			});
 
-      return this.pool
-    }
+			return this.pool;
+		}
 
-    return this.pool
-  }
+		return this.pool;
+	}
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
