12ba1962a1f291b4ca16c950f98273082abb8c82
Sam <Sam@Macintosh.local>
2019-12-07 14:32:54 -0600

    ready for turn in

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
+        host: '34.66.70.25',
         user: 'root',
-        password: 'password',
+        password: '',
         database: 'admin'
       })
 
