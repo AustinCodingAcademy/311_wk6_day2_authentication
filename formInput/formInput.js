8fd8c11e6e4572428a7423f6eec2ab13de480de4
David Bille <d_bille@yahoo.com>
2019-12-08 01:39:57 -0600

    working on post function and submit button

--- formInput/formInput.js
+++ formInput/formInput.js
@@ -1,7 +1,5 @@
 const button = document.querySelector('#button');
-button.addEventListener('click', function (){
-  console.log('howdy')
-})
+button.addEventListener('click', signup)
 
 function signup (e) {
   const username = document.querySelector('#username').value;
@@ -15,5 +13,9 @@ function signup (e) {
       password: password,
     }
   }
+  fetch('localhost:4001/auth/signup', newUser)
+  .then((res) => {
+    console.log('Successfully ceated user', res.json())
+  })
   
 };
