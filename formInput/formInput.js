34f7be28117e2dff4ef463f0e5d8e62af02c25d6
David Bille <d_bille@yahoo.com>
2019-12-08 01:43:40 -0600

    need to figure out how to link form functionality to app.js

--- formInput/formInput.js
+++ formInput/formInput.js
@@ -1,21 +1,19 @@
-const button = document.querySelector('#button');
-button.addEventListener('click', signup)
+const button = document.querySelector("#button");
+button.addEventListener("click", signup);
 
-function signup (e) {
-  const username = document.querySelector('#username').value;
-  const password = document.querySelector('#password').value;
+function signup(e) {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
   e.preventDefault();
   const newUser = {
-    method: 'POST',
+    method: "POST",
     headers: { "Content-Type": "application/json" },
     body: {
       username: username,
-      password: password,
+      password: password
     }
-  }
-  fetch('localhost:4001/auth/signup', newUser)
-  .then((res) => {
-    console.log('Successfully ceated user', res.json())
-  })
-  
-};
+  };
+  fetch("localhost:4001/auth/signup", newUser).then(res => {
+    console.log("Successfully created new account!", res.json());
+  });
+}
