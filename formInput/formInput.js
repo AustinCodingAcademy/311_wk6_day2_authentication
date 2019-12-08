f50eb6f6cb04579ca8f4131c6311c744eec20c00
David Bille <d_bille@yahoo.com>
2019-12-08 01:33:03 -0600

    commented code

--- formInput/formInput.js
+++ formInput/formInput.js
@@ -6,10 +6,9 @@ button.addEventListener('click', function (){
 function signup (e) {
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
-  console.log('hey we are connedcted')
   e.preventDefault();
   const newUser = {
-    // method: 'POST',
+    method: 'POST',
     headers: { "Content-Type": "application/json" },
     body: {
       username: username,
