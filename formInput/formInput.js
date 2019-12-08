5de3f544d98b4fb63efc60739d2f53ff65143d9a
David Bille <d_bille@yahoo.com>
2019-12-08 01:31:05 -0600

    still trying to figure it out

--- formInput/formInput.js
+++ formInput/formInput.js
@@ -1,9 +1,15 @@
-signUp = e => {
+const button = document.querySelector('#button');
+button.addEventListener('click', function (){
+  console.log('howdy')
+})
+
+function signup (e) {
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
+  console.log('hey we are connedcted')
   e.preventDefault();
   const newUser = {
-    method: 'POST',
+    // method: 'POST',
     headers: { "Content-Type": "application/json" },
     body: {
       username: username,
--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const { logger } = require("./middleware/index");
 const app = express();
 const port = process.env.PORT || 4001;
 
-// app.use(express.static('index.html'))
+app.use(express.static("./formInput"));
 app.get("/", (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile("index.html", { root: __dirname });
 });
 
 app.use(bodyParser.json());
 app.use(express.static("index"));
 app.use("/users", logger, usersRouter);
 app.use("/auth", authRouter);
-
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
