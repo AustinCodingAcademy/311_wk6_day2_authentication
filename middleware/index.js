3a4f02952837687a0815c783ef927995d4a0127e
jlmeister <iamphil4483@yahoo.com>
2019-12-08 16:11:39 -0600

    created authentication middleware for an express API. final commit.

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,23 @@
 const jwt = require('jsonwebtoken')
 
-const logger = () => {}
+const logger = (req, res, next) => {
+  console.log('Logging route: ', req.method, req.path, new Date().toISOString());
+  next();
+}
 
-const authenticate = () => {}
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  if (typeof authHeader !== 'undefined') {
+    const bearer = authHeader.split(' ');
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, 'shhhhh', (err, decoded) => {
+      req.user = decoded;
+      next();
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+}
 
 module.exports = {
   logger,
