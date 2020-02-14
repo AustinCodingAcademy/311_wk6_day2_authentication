c3d8289db3d039240b00ac98c02b5da252ea754d
Pace Davis <pacemdavis@gmail.com>
2020-02-13 20:18:53 -0600

    working

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,10 @@
 const jwksRsa = require('jwks-rsa');
 const jwt = require('express-jwt');
 
-const logger = () => {}
+const logger = (req, res, next) => {
+  console.log('Logging route:', './users', new Date().toISOString());
+  next();
+}
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -15,6 +18,7 @@ const checkJwt = jwt({
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
+
 
 module.exports = {
   logger,
