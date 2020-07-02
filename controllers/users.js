f1937fc9b6bfc247e41bd7bb1bdae1dd1c920c37
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-01 20:56:18 -0500

    Updates

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
-const getAllUsers = (req, res) => {git 
+const getAllUsers = (req, res) => {
   pool.query("SELECT * FROM users", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
