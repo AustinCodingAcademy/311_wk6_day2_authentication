c40307a623269a0398fec1eee4c577de4cab310a
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-01 20:33:54 -0500

    Updates

--- controllers/users.js
+++ controllers/users.js
@@ -9,15 +9,15 @@ const getAllUsers = (req, res) => {
   })
 }
 
-const getUserById = (req, res) => {
-  let sql = "SELECT * FROM users WHERE id = ?"
-  sql = mysql.format(sql, [ req.params.id ])
+// const getUserById = (req, res) => {
+//   let sql = "SELECT * FROM users WHERE id = ?"
+//   sql = mysql.format(sql, [ req.params.id ])
 
-  pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
-  })
-}
+//   pool.query(sql, (err, rows) => {
+//     if (err) return handleSQLError(res, err)
+//     return res.json(rows);
+//   })
+// }
 
 const createUser = (req, res) => {
   res.json("hi mom")
