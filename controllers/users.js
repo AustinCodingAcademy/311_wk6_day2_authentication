03381ab2235733bb43b606475a625ba89dab5673
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-01 20:43:55 -0500

    Hi mom

--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,11 @@ const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
-  pool.query("SELECT * FROM users", (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
-  })
+  res.json("hi mom")
+  // pool.query("SELECT * FROM users", (err, rows) => {
+  //   if (err) return handleSQLError(res, err)
+  //   return res.json(rows);
+  // })
 }
 
 // const getUserById = (req, res) => {
