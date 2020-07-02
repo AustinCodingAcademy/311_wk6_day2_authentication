fc359c0ff4a502dee8ab66357395a0efa19fddb4
rodrieri000 <54131476+rodrieri000@users.noreply.github.com>
2020-07-01 19:49:00 -0500

    Update connection.js

--- sql/connection.js
+++ sql/connection.js
@@ -6,10 +6,10 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.223.0.16',
-        user: 'root',
-        password: '89429777ers',
-        database: 'Admin'
+        host: 'Den1.mysql1.gear.host',
+        user: 'jamatx',
+        password: '&*&$!89429777ers',
+        database: 'jamatx'
       })
 
       return this.pool
