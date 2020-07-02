931e4765fa5713ded94981f3af0b9a696692411b
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-01 20:18:40 -0500

    Router updates

--- routers/users.js
+++ routers/users.js
@@ -5,12 +5,12 @@ const router = express.Router()
 
 // router.get('/', usersController.getAllUsers)
 
-router.get('/:id', usersController.getUserById)
+router.get('/getUser/:id', usersController.getUserById)
 
-router.post('/', checkJwt, usersController.createUser)
+router.post('/createUser', checkJwt, usersController.createUser)
 
-router.put('/:id', checkJwt, usersController.updateUserById)
+router.put('/updateUser/:id', checkJwt, usersController.updateUserById)
 
-router.delete('/:first_name', checkJwt, usersController.deleteUserByFirstName)
+router.delete('/deleteUser/:first_name', checkJwt, usersController.deleteUserByFirstName)
 
 module.exports = router
