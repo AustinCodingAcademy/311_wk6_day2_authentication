0965608cce2c3c1277c72f0956f2e10b113128f8
Ben Edwards <benedwrds@yahoo.com>
2020-07-12 11:56:19 -0500

    password

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.193.239.243',
-        user: 'root',
-        password: 'Skate123!',
+        host: 'host',
+        user: 'usergit',
+        password: 'passtheword',
         database: 'admin'
       })
 
