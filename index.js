f9f7cfe489c68f0b488a1ce99163afa844e290fd
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-13 20:29:28 -0500

    Added cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const usersRouter = require('./routers/users');
 const authRouter = require('./routers/auth');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 4001;
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
