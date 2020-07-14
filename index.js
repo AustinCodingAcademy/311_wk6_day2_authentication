0b1b55521ffbc81b61135ac9b760caceb2d052d2
rodrieri000 <ericarodriguez7@outlook.com>
2020-07-13 20:52:07 -0500

    Updates

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const port = process.env.PORT || 4001;
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
